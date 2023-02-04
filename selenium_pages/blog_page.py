65374a0fe62827e4ea9a6d2741fc92c4352895d3
RG <ForUiPathacc94@proton.me>
2023-02-04 22:46:32 +0800

    Add blog_page.py

---
+++ selenium_pages/blog_page.py
@@ -0,0 +1,20 @@
+from selenium_pages.base_page import BasePage
+from selenium_locators import sel_locators as l
+
+
+class BlogPage(BasePage):
+    def __init__(self, driver, login_cookies):
+        super().__init__(driver)
+        self.get_blog_page()
+        self._driver.add_cookie(login_cookies)
+        # self._driver.refresh()
+
+
+
+    def get_blog_page(self):
+        self.get_url(l.BLOG_PAGE)
+
+    def check_test_post(self, title="Test"):
+        return self.find_element(
+            (l.TEST_POST[0], l.TEST_POST[1].format(title))
+        )
