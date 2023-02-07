d52c763d60dd2bb2220af7cf00a7c5a565e9843c
RG <ForUiPathacc94@proton.me>
2023-02-07 23:48:34 +0800

    Change cart_page.py

--- selenium_pages/cart_page.py
+++ selenium_pages/cart_page.py
@@ -1,34 +1,57 @@
 import time
 from random import randint
-
 from selenium_pages.base_page import BasePage
 from selenium_locators import sel_locators as l
 
 
 class CartPage(BasePage):
-    def __init__(self, driver, login_cookies):
+    def __init__(self, driver):
         super().__init__(driver)
-        self.get_page()
-        self._driver.add_cookie(login_cookies)
 
-    def get_page(self):
+
+    def get_cart_page(self):
         self.get_url(l.CART_PAGE)
 
     def get_shop_page(self):
         self.get_url(l.SHOP_PAGE)
 
-    def add_product_random(self):
-        id_product = randint(223, 232)
+    def randint(self) -> int:
+        id_product = randint(224, 232)
         if id_product >= 229:
             id_product += 1
-        print(id_product)
-        self.actionChains.move_to_element(
-            self.wait_for(
-                *((l.HOVER_PRODUCT[0], l.HOVER_PRODUCT[1].format(id_product)))
-            )
-        ).perform()
-        time.sleep(2)
+        return id_product
+
+    def random_list_products(self, count_int) -> set:
+        new_set = set()
+        while len(new_set) != count_int:
+            id_product = self.randint()
+            new_set.add(id_product)
+        return new_set
+
+
+    def add_product(self, list_products):
+        for id_product in list_products:
+
+            element = self.find_element(
+                    (l.HOVER_PRODUCT[0], l.HOVER_PRODUCT[1].format(id_product))
+                )
+
+            self.driver().execute_script("arguments[0].scrollIntoView(true);", element)
+            self.actionChains.move_to_element(element).perform()
+
+            self.find_element(
+                (l.ADD_PRODUCT[0], l.ADD_PRODUCT[1].format(id_product))
+            ).click()
+            self.find_element(l.CLOSE_CART_MENU).click()
+
+    def cart_size(self) -> int:
+        return len(self.find_elements(l.CART_SIZE))
+
+    def cart_remove_product(self):
+        size = self.cart_size()
         self.find_element(
-            (l.ADD_PRODUCT[0], l.ADD_PRODUCT[1].format(id_product))
+            (l.REMOVE_PRODUCT[0], l.REMOVE_PRODUCT[1].format(size))
         ).click()
-        time.sleep(1)
+
+
+
