7e9d384a416ef3143d888f93024b50624956339d
RG <ForUiPathacc94@proton.me>
2023-02-07 23:49:18 +0800

    Change base_page.py

--- selenium_pages/base_page.py
+++ selenium_pages/base_page.py
@@ -1,5 +1,5 @@
 from selenium.webdriver import ActionChains
-from selenium_locators.sel_locators import overlay
+from selenium_locators.sel_locators import overlay, blackOverlay
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support.ui import WebDriverWait
@@ -10,12 +10,13 @@ from configuration import BASE_URL
 class BasePage:
     def __init__(self, driver: WebDriver):
         self._driver = driver
-        self._wait = WebDriverWait(self._driver, 30)
+        self._wait = WebDriverWait(self._driver, 20)
         self.actionChains = ActionChains(driver)
         self.default_url = BASE_URL
         self.overlay = overlay
+        self.blackOverlay = blackOverlay
 
-    def driver(self):
+    def driver(self) -> WebDriver:
         return self._driver
 
     def wait_for(self, *locator: dict) -> WebDriver:
@@ -23,12 +24,17 @@ class BasePage:
 
     def _wait_for_overlay(self, locator: dict) -> dict:
         self._wait.until(ec.invisibility_of_element_located(self.overlay))
+        self._wait.until(ec.invisibility_of_element_located(self.blackOverlay))
         self._wait.until(ec.presence_of_element_located(locator))
+        self._wait.until(ec.visibility_of_element_located(locator))
         self._wait.until(ec.visibility_of_element_located(locator))
         return locator
 
     def find_element(self, locator) -> WebElement:
         return self._driver.find_element(*(self._wait_for_overlay(locator)))
+
+    def find_elements(self, locator) -> list[WebElement]:
+        return self._driver.find_elements(*(self._wait_for_overlay(locator)))
 
     def get_url(self, url: str):
         self._driver.get(url)
