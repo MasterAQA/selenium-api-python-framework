f50b42af800357b575ffa7fb2ebf23461ad8255d
RG <ForUiPathacc94@proton.me>
2023-02-07 23:48:41 +0800

    Change test_cart.py

--- tests/test_cart.py
+++ tests/test_cart.py
@@ -1,9 +1,38 @@
 import time
 
 
-# def test_cart(api_client, cart_page):
-#     cart_page.get_shop_page()
-#     cart_page.add_product_random()
-#     time.sleep(2)
-#     cart_page.add_product_random()
-#     time.sleep(2)
+def test_cart_add(api_client, cart_page):
+    cart_page.get_shop_page()
+    cart_page.add_product()
+    cart_page.get_cart_page()
+
+    assert cart_page.cart_size() == 1
+
+def test_cart_add_multiple(api_client, cart_page):
+    cart_page.get_shop_page()
+    [cart_page.add_product() for _ in range(3)]
+    cart_page.get_cart_page()
+
+    assert cart_page.cart_size() == 3
+
+def test_cart_remove(api_client, cart_page):
+    cart_page.get_shop_page()
+    id_products = cart_page.random_list_products(3)
+    cart_page.add_product(id_products)
+    cart_page.get_cart_page()
+    cart_page.cart_remove_product()
+
+    assert cart_page.cart_size() == 2
+
+
+def test_cart_remove_multiple(api_client, cart_page):
+    cart_page.get_shop_page()
+    id_products = cart_page.random_list_products(7)
+    cart_page.add_product(id_products)
+    cart_page.get_cart_page()
+    [cart_page.cart_remove_product() for _ in range(3)]
+
+    assert cart_page.cart_size() == 4
+
+
+
