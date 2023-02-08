e51f66c416a4d97b1504e8db10efae61002d589f
RG <ForUiPathacc94@proton.me>
2023-02-08 22:28:09 +0300

    Reduced using parametrize

--- tests/test_product_categories.py
+++ tests/test_product_categories.py
@@ -1,3 +1,5 @@
+import pytest
+
 CATEGORY_ACCESSORIES = "Accessories"
 CATEGORY_CLOTHING = "Clothing"
 CATEGORY_DECOR = "Decor"
@@ -10,61 +12,23 @@ CATEGORY_SALE_PRODUCTS = "Sale Products"
 CATEGORY_TSHIRTS = "Tshirts"
 
 
-def test_category_accessories(home_page):
-    home_page.click_category(CATEGORY_ACCESSORIES)
+@pytest.mark.parametrize(
+    "category",
+    [
+        CATEGORY_ACCESSORIES,
+        CATEGORY_CLOTHING,
+        CATEGORY_DECOR,
+        CATEGORY_FASHION,
+        CATEGORY_HOODIES,
+        CATEGORY_MOST_POPULAR,
+        CATEGORY_MUSIC,
+        CATEGORY_NEW_ARRIVALS,
+        CATEGORY_SALE_PRODUCTS,
+        CATEGORY_TSHIRTS,
+    ],
+)
+def test_categories(home_page, category):
+    home_page.click_category(category)
 
-    assert home_page.title_category(CATEGORY_ACCESSORIES)
+    assert home_page.title_category(category)
 
-
-def test_category_clothing(home_page):
-    home_page.click_category(CATEGORY_CLOTHING)
-
-    assert home_page.title_category(CATEGORY_CLOTHING)
-
-
-def test_category_decor(home_page):
-    home_page.click_category(CATEGORY_DECOR)
-
-    assert home_page.title_category(CATEGORY_DECOR)
-
-
-def test_category_fashion(home_page):
-    home_page.click_category(CATEGORY_FASHION)
-
-    assert home_page.title_category(CATEGORY_FASHION)
-
-
-def test_category_hoodies(home_page):
-    home_page.click_category(CATEGORY_HOODIES)
-
-    assert home_page.title_category(CATEGORY_HOODIES)
-
-
-def test_category_most_popular(home_page):
-    home_page.click_category(CATEGORY_MOST_POPULAR)
-
-    assert home_page.title_category(CATEGORY_MOST_POPULAR)
-
-
-def test_category_music(home_page):
-    home_page.click_category(CATEGORY_MUSIC)
-
-    assert home_page.title_category(CATEGORY_MUSIC)
-
-
-def test_category_new_arrivals(home_page):
-    home_page.click_category(CATEGORY_NEW_ARRIVALS)
-
-    assert home_page.title_category(CATEGORY_NEW_ARRIVALS)
-
-
-def test_category_sale_products(home_page):
-    home_page.click_category(CATEGORY_SALE_PRODUCTS)
-
-    assert home_page.title_category(CATEGORY_SALE_PRODUCTS)
-
-
-def test_category_tshirts(home_page):
-    home_page.click_category(CATEGORY_TSHIRTS)
-
-    assert home_page.title_category(CATEGORY_TSHIRTS)
