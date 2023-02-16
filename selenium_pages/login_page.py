525476db604a5dc5af0c26e007f0cf525db0d4f5
RG <ForUiPathacc94@proton.me>
2023-02-16 23:43:05 +0300

    Reduce locators

--- selenium_pages/login_page.py
+++ selenium_pages/login_page.py
@@ -1,5 +1,5 @@
 from selenium_pages.base_page import BasePage
-from selenium_pages import sel_locators
+from selenium_pages import sel_locators as l
 
 
 class LoginPage(BasePage):
@@ -8,28 +8,28 @@ class LoginPage(BasePage):
         self.get_page()
 
     def get_page(self):
-        self.get_url(sel_locators.LOGIN_PAGE)
+        self.get_url(l.LOGIN_PAGE)
 
     def login(self, email, password):
-        self.find_element(sel_locators.USERNAME_LOGIN).send_keys(email)
-        self.find_element(sel_locators.PASSWORD_LOGIN).send_keys(password)
-        self.find_element(sel_locators.LOGIN_BUTTON).click()
+        self.find_element(l.USERNAME_LOGIN).send_keys(email)
+        self.find_element(l.PASSWORD_LOGIN).send_keys(password)
+        self.find_element(l.LOGIN_BUTTON).click()
 
     def good_login(self) -> bool:
-        return self.find_element(sel_locators.WELCOME_GOOD).is_displayed()
+        return self.find_element(l.WELCOME_GOOD).is_displayed()
 
     def error_login(self) -> bool:
-        return self.find_element(sel_locators.WELCOME_ERROR).is_displayed()
+        return self.find_element(l.WELCOME_ERROR).is_displayed()
 
     def good_registration(self, email, password):
-        self.find_element(sel_locators.USERNAME_REG).send_keys(email)
-        self.find_element(sel_locators.PASSWORD_REG).send_keys(password)
-        self.find_element(sel_locators.REG_BUTTON).click()
+        self.find_element(l.USERNAME_REG).send_keys(email)
+        self.find_element(l.PASSWORD_REG).send_keys(password)
+        self.find_element(l.REG_BUTTON).click()
 
     def bad_registration(self, email, password):
-        self.find_element(sel_locators.USERNAME_REG).send_keys(email)
-        self.find_element(sel_locators.PASSWORD_REG).send_keys(password)
-        self.find_element(sel_locators.REG_BUTTON).click()
+        self.find_element(l.USERNAME_REG).send_keys(email)
+        self.find_element(l.PASSWORD_REG).send_keys(password)
+        self.find_element(l.REG_BUTTON).click()
 
     def reg_button_available(self) -> bool:
-        return self.find_element(sel_locators.REG_BUTTON).is_enabled()
+        return self.find_element(l.REG_BUTTON).is_enabled()
